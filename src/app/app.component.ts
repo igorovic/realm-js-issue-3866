e531204427fceb8b19eef81e9f206347e07acb8e
igorovic <hello@dyve.dev>
2021-08-13 10:46:26 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { App } from 'realm-web';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-realm-web';
+  app = new App({ id: 'anything-here' });
 }
